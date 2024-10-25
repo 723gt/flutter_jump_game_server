47689d638762421d8dbe618f5b779f02033a105f
yoshioka <rb.natsumi@gmail.com>
2024-10-25 22:14:34 +0900

    remove console.log

--- src/server/index.ts
+++ src/server/index.ts
@@ -35,7 +35,6 @@ io.on("connection", (socket) => {
     socket.join(roomId);
   });
   socket.on("info", (params) => {
-    console.log("join-info");
     const userInfo: UserInfo = params as UserInfo;
     addUserInfo(roomId,socket.id, userInfo);
     io.to(roomId).emit("user-join-info", getUserInfos(roomId));
@@ -53,7 +52,6 @@ io.on("connection", (socket) => {
 
   socket.on("start", params => {
     const startReadyUser = getUserInfo(socket.id);
-    console.log("start");
     if(startReadyUser != null){
       const duplicate = readyUsers.filter((f) => f.clientId == startReadyUser.clientId);
       if(duplicate.length === 0){
@@ -76,7 +74,6 @@ io.on("connection", (socket) => {
       }
     }
     if(isGameSet(roomId)){
-      console.log("stop!!!");
       stopBullet();
       io.to(roomId).emit("gameSet", {});
       gameOverUsers = [];
@@ -87,7 +84,6 @@ io.on("connection", (socket) => {
     if (intervalId === null) {
         intervalId = setInterval(() => {
         io.to(roomId).emit("bullet",{});
-        console.log('hello world');
         }, 5000);
     }
   }
@@ -115,16 +111,11 @@ function addUserInfo(roomId: string, id: string, userInfo: UserInfo): Array<Serv
 
 function isGameSet(roomId: string): boolean{
   const userInfos = getUserInfos(roomId);
-  console.log("isGameSet")
-  console.log(gameOverUsers);
-  console.log(isUsersEqual(userInfos, gameOverUsers))
   return isUsersEqual(userInfos, gameOverUsers);
 }
 
 function isGameStart(roomId: string):boolean{
   const userInfo = getUserInfos(roomId);
-  console.log("isGameStart")
-  console.log(isUsersEqual(userInfo, readyUsers));
   return(isUsersEqual(userInfo, readyUsers));
 }
 
